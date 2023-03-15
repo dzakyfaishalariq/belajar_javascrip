88f2cceaf003bc2ca8bb80fd43fb4e2cc39d2c4e
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 17:55:23 +0700

    Menggunakan Math fungsi

---
+++ file_js/index5.js
@@ -0,0 +1,9 @@
+// menggunakan fungsi Math
+console.log(Math.PI) // menggunakan nilai Pi
+console.log(Math.E)// nilai E
+console.log(Math.abs(-123))
+console.log(Math.abs(123)) // nilai absolud
+console.log(Math.floor(1.8)) //pembulatan ke bawah
+console.log(Math.round(2.7)) // pembulatan ke atas
+a = Math.round(Math.random()*100)
+console.log(a)
