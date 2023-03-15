947be11074d4734bb2dd257a2dcdaefd437abc38
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 17:31:50 +0700

    Menggunakan fungsi yang ada di dalam js

---
+++ file_js/index3.js
@@ -0,0 +1,7 @@
+// menggunakan fungsi yang ada di dalam javascrip
+let a = "ini aDaLah Variabel"
+let b = " ini skat  "
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(b.trim().toUpperCase())
+
