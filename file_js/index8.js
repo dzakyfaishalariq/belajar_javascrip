4ada0ccf97606930e535e8f2ed8d3906a31eb091
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 22:03:51 +0700

    Menggunakan if stetmen satu pilihan

---
+++ file_js/index8.js
@@ -0,0 +1,10 @@
+// if stetment
+let a = Math.round(Math.random()*100)
+
+console.log(a)
+if (a >= 50){
+    console.log(`data lebih besar atau sama dengan 50 <= ${a}`)
+}
+if (a <= 50){
+    console.log(`data lebih kecil atau sama dengan 50 >= ${a}`)
+}
