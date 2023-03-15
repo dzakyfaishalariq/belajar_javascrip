e8fd9ac506077593240dc30cce3bee527c0bb42f
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 20:24:16 +0700

    Menggunakan perbandingan di dalam js

---
+++ file_js/index6.js
@@ -0,0 +1,30 @@
+// operator perbandingan
+let a = 10, b = 11, c = -10
+
+per1 = a < b
+per2 = a > c
+per3 = (a == b)
+per9 = (a != b)
+per10 = (a != c)
+per4 = (a===b)
+per5 = a <= b
+per6 = a >= c
+per7 = a !== c
+per8 = b !== c
+
+console.log(per1)
+console.log("===")
+console.log(per2)
+console.log("===")
+console.log(per3)
+console.log("===")
+console.log(per4)
+console.log(per5)
+console.log(per6)
+console.log(per7)
+console.log("===")
+console.log(per8)
+console.log(per9)
+console.log(per10)
+// penggunaaan dalam signifikan tenteang === atau == penjelasan:
+// -> dalam menggunakan prinsip === atau == utamkan yang === karena dia membandingkan dengan variabel lain dengan signifikan
