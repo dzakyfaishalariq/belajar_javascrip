fdce2d6bdbb18ba2869a9c4e39d9182aaae6d868
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 17:39:16 +0700

    Menggunakan fungsi yang ada di dalam js

--- file_js/index3.js
+++ file_js/index3.js
@@ -5,3 +5,8 @@ console.log(a.toUpperCase())
 console.log(a.toLowerCase())
 console.log(b.trim().toUpperCase())
 
+console.log(a.split(' '))
+console.log(a.indexOf('D'))
+console.log(a.slice(4,7))
+console.log(a.replace('aDa','ada'))
+
