b93295d1433e150b818b25855ac0a9f453e4ac58
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 21:44:45 +0700

    Menggunakan console, alert, dan prompt

---
+++ file_js/index7.js
@@ -0,0 +1,10 @@
+// mempelajari console alert
+console.log(123)
+console.error("ini terjadi kesalahan") // pesan error
+console.warn('Hati-hati ya ! ') // pesan warning
+
+// penggunaan alert
+alert("hallo apa kabar")
+// penggunaa prompt
+nama = prompt('masukan nama anda : ')
+alert(`nama anda : ${nama}`)
