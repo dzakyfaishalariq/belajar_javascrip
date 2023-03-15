af16f45585561d78af1cc2d0f718e98948b84d86
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 17:27:33 +0700

    belajar javascrip

---
+++ file_js/index2.js
@@ -0,0 +1,12 @@
+// tipe data string
+console.log('hallo apa kabar')
+console.log('hallo apa "kabar"')
+console.log("hallo 'apa' kabar")
+console.log(typeof ('20'))
+console.log(typeof (20))
+
+a = 'hallo apa kabar'
+console.log(a[4])
+console.log(a + " ? ")
+console.log(a)
+
