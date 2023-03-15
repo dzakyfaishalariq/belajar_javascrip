c580c619ec19c19b0e536ecf0abb337ba2603ad6
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 22:20:04 +0700

    Menggunakan if dan else stetmen

---
+++ file_js/index9.js
@@ -0,0 +1,26 @@
+const inihari = prompt('masukan hari anda:').toLowerCase()
+
+if (inihari === "senin"){
+    console.log("ini hari senin untuk sekolah")
+}else if (inihari === "sabtu"){
+    console.log('Hooree hari sabtu')
+}else {
+    console.log("tidak ada hari sepsial kecuali jumat")
+}
+
+// const  nilai = 90
+// if (nilai  < 50){
+//     console.log("C")
+// }else if (nilai < 60){
+//     console.log("-B")
+// }else if (nilai < 70){
+//     console.log("B")
+// }else if (nilai < 80){
+//     console.log("B+")
+// }else if (nilai < 90){
+//     console.log("-A")
+// }else if (nilai < 100){
+//     console.log("A")
+// }else {
+//     console.log("D")
+// }
