727fac6d774fee8367ca54368fafa834a9598884
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-15 17:45:05 +0700

    Menggunakan tempalating variabel ke string

---
+++ file_js/index4.js
@@ -0,0 +1,8 @@
+// mengunakan templating string
+let a = "hallo"
+let b = " apa kabar"
+let nama = "Dzaky Faishlariq"
+let npm = "G1A020003"
+console.log(a + b)
+console.log(`nama : ${nama}`)
+console.log(`npm : ${npm}`)
