efc69589bc48ffaa9628c5a08321f8c30decdbbf
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-16 20:35:45 +0700

    Pembelajaran operator &&, ||, !=

--- file_js/index11.js
+++ file_js/index11.js
@@ -2,8 +2,21 @@
 
 // maka penggunaannya
 const password = '1234indigo pin'
+const userRole = 'admin'
 if (password.length >= 6 && password.indexOf(' ') === -1){
     console.log('password valid')
 }else {
     console.log('password kurang valid')
+}
+
+if(userRole === 'admin' || userRole === 'spv'){
+    console.log('boleh mengakses')
+}else {
+    console.log('tidak boleh mengakses')
+}
+
+if(userRole !== 'admin'){
+    console.log('akses di terima')
+}else {
+    console.log('akses di tolak')
 }
