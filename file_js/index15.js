3c11da635230d71c8abbc7c616212ed14ea6e164
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-16 21:55:02 +0700

    Pembelajaran objek dalam array

---
+++ file_js/index15.js
@@ -0,0 +1,9 @@
+const a = [
+    {nama:'Anis',npm:'002'},
+    {nama:'Beni',npm:'003'},
+    {nama:'Cagur',npm:'004'},
+    {nama:'Koni',npm:'005'},
+]
+console.log(a)
+console.log(a[0])
+console.log(a[0].nama)
