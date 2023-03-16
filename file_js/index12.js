adf65360884ad75b525a71a5e23cb58b51b9ab07
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-16 20:40:24 +0700

    Pembelajaran switch

---
+++ file_js/index12.js
@@ -0,0 +1,13 @@
+console.log("pengenalan switch")
+
+const namaHari = "Minggu"
+switch (namaHari){
+    case "Senin":
+        console.log('hari sibuk')
+        break
+    case "Selasal":
+        console.log('hari santai')
+        break
+    default:
+        console.log('hari enak')
+}
