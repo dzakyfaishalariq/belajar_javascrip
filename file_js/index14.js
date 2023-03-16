4787644dd2e1527a8c809f995cb6af69a1e302e9
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-16 21:42:56 +0700

    Pembelajaran data objek

---
+++ file_js/index14.js
@@ -0,0 +1,16 @@
+const identitas ={
+    nama:'Dzaky Faishalariq',
+    npm : 'G1A020003',
+    semester:4,
+    hobi : [
+        'sains',
+        'teknologi'
+    ]
+}
+console.log(identitas)
+console.log(identitas.hobi)
+console.log(identitas.hobi[1])
+
+identitas.nama = "Fahri"
+identitas.npm = 'G1A020004'
+console.log(identitas)
