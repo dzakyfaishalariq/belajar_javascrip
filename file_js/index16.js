37694c1ff1015b0cf16a4624d90f41409ab960b6
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-17 17:18:37 +0700

    Pembelajaran perulangan

---
+++ file_js/index16.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i <= 10; i++){
+//     console.log(`ini index ${i}`)
+// }
+for (let i = 0; i <= 20; i++){
+    if (i % 2 === 0){
+        console.log(`genab : ${i}`)
+    }
+}
+console.log("=====")
+for (let i = 20; i >= 0; i--){
+    if(i % 2 !== 0){
+        console.log(`ganjil : ${i}`)
+    }
+}
