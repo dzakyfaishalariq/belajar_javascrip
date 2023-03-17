ea6f37e8df90acbd2832ff5a787aa5eab6a6aa52
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-17 17:53:42 +0700

    Pembelajaran brek dalam perulangan

---
+++ file_js/index20.js
@@ -0,0 +1,11 @@
+// let bicara = prompt('katakan sesuatu : ')
+// while (true){
+//     bicara = prompt(bicara)
+//     if (bicara.toLowerCase() === 'stop') break;
+// }
+// alert('oke sudah katakan')
+
+for (let i = 0; i < 1000; i++){
+    console.log(i+1)
+    if (i === 100) break;
+}
