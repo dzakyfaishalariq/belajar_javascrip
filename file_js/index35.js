4dd6219c6788a73eaf520e95dd73e586f0de9935
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-19 22:15:38 +0700

    penggunaan implisit function

---
+++ file_js/index35.js
@@ -0,0 +1,17 @@
+// fungsi explisit
+
+const tambah = (a,b)=>{
+    return a+b
+}
+console.log(tambah(1,5))
+
+// secara implisit
+
+const kali = (a,b) => (
+    a*b
+)
+console.log(kali(2,9))
+
+const bagi = (a,b) => a/b;
+
+console.log(bagi(8,4))
