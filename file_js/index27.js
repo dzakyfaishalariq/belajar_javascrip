e2f8c1c0cb3225965932b5d5fe4e79390f65dc89
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-03-19 20:42:10 +0700

    function expresion

---
+++ file_js/index27.js
@@ -0,0 +1,12 @@
+const pangkat2 = (nilai) => {
+    return nilai*nilai
+}
+
+const pangkat3 = function (nilai){
+    return nilai*nilai*nilai
+}
+let nilai,nilai2
+nilai = Math.sqrt(9) //akar
+console.log(pangkat2(2))
+console.log(pangkat3(3))
+console.log(nilai)
